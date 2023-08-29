0dc8222e6b329bfdb8deab27a652c275931568a7
Miguel M <miguel.maldonado5802@gmail.com>
2023-08-29 08:29:03 -0400

    got the form to submit the user info, used the geocoding to take the city name and then send it to the weather api to get that city weather. Working on displaying the rest of the information.

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,35 +1,50 @@
 // api
 // https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}&units=imperial
 
+// weather api geocoding
+// http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
+
 var form = document.querySelector("#search-form")
-var searchInput = document.querySelector('#search')
+var userCityInput = document.querySelector('#search')
 
-// need to have the api get called. Then need it to be spliced into useable sections. 
-// first work on making sure the API works
-// second work off the submit function. 
-
+// event listener to the submit of the form with the user input
 form.addEventListener('submit', function(event) {
     event.preventDefault();
-    var userinput = searchInput.value;
-    
-    weatherAPI(userinput);
+    var userCity = userCityInput.value;
+
+    locationApi(userCity);
 }) 
 
-var weatherAPI = function (user) {
-    var apiUrl = 'https://api.openweathermap.org/data/2.5/weather?lat=35.91&lon=-82.30&appid=4ff4b45b9265dee6811bebe35a9085ea&units=imperial'
-    // var apiUrl = https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}&units=imperial
-    // 35.918896030764245, -82.30132160280368
+// function that uses the openweather geocoding to take the user input and then get the lat and long from it
+var locationApi = function(city) {
+    var userlocal = 'http://api.openweathermap.org/geo/1.0/direct?q=' + city + '&limit=1&appid=4ff4b45b9265dee6811bebe35a9085ea'
+    fetch(userlocal)
+        .then(function (response) {
+            return response.json()
+        })
+        .then(function (data) {
+            var lat = data[0].lat
+            var long = data[0].lon
+
+            weatherAPI(lat, long)
+        }) 
+       
+} 
+
+// taking the users modified input and then getting the weather information from that
+var weatherAPI = function (lat, long) {
+    var apiUrl = 'https://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + long + '&appid=4ff4b45b9265dee6811bebe35a9085ea&units=imperial'
+    console.log(apiUrl)
 
     fetch(apiUrl)
         .then(function (response) {
-            return response.json()
-            
-            .then(function (data) {
-            console.log(data.main.humidity)
-            console.log(data.weather[0].icon)
-            console.log(data)
+            return response.json();
         })
-        })
-}
+        .then(function (data) {
+        console.log(data.main.humidity);
+        console.log(data.weather[0].icon);
+        console.log(data);
+    });
+};
 
-weatherAPI()
+// weatherAPI()
